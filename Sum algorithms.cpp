1047a1084e4a6f896e403cc22b38019401bdea97
sambhavu <30390294+sambhavu@users.noreply.github.com>
2023-03-03 22:46:25 -0500

    Create Sum algorithms.cpp

---
+++ Sum algorithms.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //algorithm to calculate the sum from 1 to 100 
+    
+    int sum = 0;
+    
+    for(int i =0; i<100; i++)
+        sum+=i
+        
+    cout<<sum;
+    
+
+    return 0;
+}
